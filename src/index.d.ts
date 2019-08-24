1658d1ed86b287e0d831f5db476b27b0161d93b6
recover758126 <761647716@qq.com>
2019-08-24 16:53:17 +0800

    fix: type declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,41 +1,22 @@
-import * as Rax from 'rax';
-import {BaseProps} from 'rax';
+import { Component, CSSProperties, ComponentClass } from 'rax';
 
-/**
- * component: icon(图标)
- * document address(文档地址)：
- * https://alibaba.github.io/rax/component/icon
- */
 export interface IconSource {
-  /**
-     * Picture icon url, if present, font and codePoint two properties are invalid
-     * (图片型icon的url，如果出现，则font和codePoint两个属性失效)
-     */
   uri?: string;
-
-  /**
-     * iconfont code point (iconfont的码点)
-     */
   codePoint: string;
 }
-
-export interface IconProps extends BaseProps {
-
-
-  /**
-     * icon source
-     */
+export interface IconProps {
   source: IconSource;
-
-  /**
-     * iconfont's font (iconfont的字体)
-     */
   fontFamily?: string;
-
+  style?: CSSProperties;
 }
-
-declare class Link extends Rax.Component<IconProps, any> {
+declare class Icon extends Component<IconProps, any> {
+  componentWillMount(): void;
   render(): JSX.Element;
 }
-
-export default Link;
+export default Icon;
+export interface IconFontProps {
+  name: string;
+  codePoint?: string;
+  style?: CSSProperties;
+}
+export declare function createIconSet(glyphMap: {}, fontFamily: string, uri: string): ComponentClass<IconFontProps>;
