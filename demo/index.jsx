707da80b9007f2f4fcf469b91d5baec9f0e533b3
Mega <761647716@qq.com>
2019-08-29 17:09:14 +0800

    Revert "Refactor/migarate to typescript and refactor the props"

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,26 +1,22 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { createElement, render, Component } from 'rax';
 import DU from 'driver-universal';
 import View from 'rax-view';
 import Icon, { createIconSet } from '../src/index';
 
-const IconFont = createIconSet({
-  'square_check': '\ue6d6',
-  'square': '\ue6d5'
-}, 'raxjs', 'https://at.alicdn.com/t/font_1368263_h562gmaj6yq.ttf');
+const IconFont1 = createIconSet({}, 'iconfont', 'https://at.alicdn.com/t/font_pkm0oq8is8fo5hfr.ttf');
+const IconFont2 = createIconSet({
+  hello: '\uE60f'
+}, 'iconfont', 'https://at.alicdn.com/t/font_pkm0oq8is8fo5hfr.ttf');
+const icon = 'https://gw.alicdn.com/tfs/TB1KRRuQXXXXXbwapXXXXXXXXXX-200-200.png';
 
 class Demo extends Component {
   render() {
     return (
       <View>
-        <Icon
-          uri="https://at.alicdn.com/t/font_1368263_h562gmaj6yq.ttf"
-          codePoint={'\ue6d5'}
-          fontFamily="raxjs"
-          color="red"
-        />
-        <IconFont name="square_check" />
-        <IconFont name="square" />
+        <Icon source={{uri: icon}}/>
+        <Icon fontFamily="iconfont" source={{uri: 'https://at.alicdn.com/t/font_pkm0oq8is8fo5hfr.ttf', codePoint: '\uE60f'}}/>
+        <IconFont1 codePoint={'\uE60f'}/>
+        <IconFont2 name={'hello'}/>
       </View>
     );
   }
